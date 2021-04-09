3b8b3481f24fee2290f6a7546f2cb679e4c6ad4a
Paul-Ngigi <paulkush7777@gmail.com>
2021-04-09 12:52:35 +0300

    add a method to define the properties of the object

--- user.py
+++ user.py
@@ -1,2 +1,17 @@
 class User:
     user_list = []  # Created an empty user list
+
+    def __init__(self, first_name, last_name, password, phone_number, email):
+        """
+        The __init__ method defines properties of the object
+        :param first_name:
+        :param last_name:
+        :param password:
+        :param phone_number:
+        :param email:
+        """
+        self.firstName = first_name
+        self.lastName = last_name
+        self.password = password
+        self.phoneNumber = phone_number
+        self.email = email
