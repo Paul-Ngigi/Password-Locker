c750d1d100ced5b504938bb78d874831ffbcbc8d
Paul-Ngigi <paulkush7777@gmail.com>
2021-04-09 13:11:32 +0300

    add a test to confirm if a user is added to the user list correctly

--- user-test.py
+++ user-test.py
@@ -12,8 +12,7 @@ class TestUser(unittest.TestCase):
         Set up method to run before each test cases.
         :return:
         """
-        self.new_user = User("Paul", "Ngigi", "secure2342", "0714060467",
-                             "paulkush7777@gmail.com")  # create contact object
+        self.new_user = User("Paul", "Ngigi", "0714060467", "paulkush7777@gmail.com")  # create contact object
 
     def test_init(self):
         """
@@ -22,9 +21,18 @@ class TestUser(unittest.TestCase):
         """
         self.assertEqual(self.new_user.firstName, "Paul")
         self.assertEqual(self.new_user.lastName, "Ngigi")
-        self.assertEqual(self.new_user.password, "secure2342")
         self.assertEqual(self.new_user.phoneNumber, "0714060467")
         self.assertEqual(self.new_user.email, "paulkush7777@gmail.com")
+
+    def test_save_user(self):
+        """
+        test_save_user test case tests if a user is being save correctly
+        in the user list
+        :return:
+        """
+        self.new_user.save_user()
+
+        self.assertEqual(len(User.user_list), 1)
 
 
 if __name__ == '__main__':
