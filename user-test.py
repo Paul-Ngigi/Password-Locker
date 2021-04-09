a4aff885d951387f4f379e76a38a2b23be4f14bc
Paul-Ngigi <paulkush7777@gmail.com>
2021-04-09 13:03:12 +0300

    add a test confirm the objects are initiated correctly

--- user-test.py
+++ user-test.py
@@ -15,3 +15,17 @@ class TestUser(unittest.TestCase):
         self.new_user = User("Paul", "Ngigi", "secure2342", "0714060467",
                              "paulkush7777@gmail.com")  # create contact object
 
+    def test_init(self):
+        """
+        test_init test case tests if the objects are being initiated correctly
+        :return:
+        """
+        self.assertEqual(self.new_user.firstName, "Paul")
+        self.assertEqual(self.new_user.lastName, "Ngigi")
+        self.assertEqual(self.new_user.password, "secure2342")
+        self.assertEqual(self.new_user.phoneNumber, "0714060467")
+        self.assertEqual(self.new_user.email, "paulkush7777@gmail.com")
+
+
+if __name__ == '__main__':
+    unittest.main()
