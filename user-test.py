e3de00dd85fcb2005956893f71983664441f31ed
Paul-Ngigi <paulkush7777@gmail.com>
2021-04-09 13:00:06 +0300

    add a setUp method to run before each test case

--- user-test.py
+++ user-test.py
@@ -1,2 +1,17 @@
 import unittest
 from user import User
+
+
+class TestUser(unittest.TestCase):
+    """
+    Test class that defines test cases for the User contact class behaviours
+    """
+
+    def setUp(self) -> None:
+        """
+        Set up method to run before each test cases.
+        :return:
+        """
+        self.new_user = User("Paul", "Ngigi", "secure2342", "0714060467",
+                             "paulkush7777@gmail.com")  # create contact object
+
