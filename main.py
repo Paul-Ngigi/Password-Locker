8e7373dbb33aba7b4403aff92a2d2a57c8310aea
Paul-Ngigi <paulkush7777@gmail.com>
2021-04-11 23:12:58 +0300

    add a function to save our users

--- main.py
+++ main.py
@@ -1,3 +1,17 @@
 from user import User
 from credentials import Credential
 
+
+def create_user(username, password, email):
+    """
+    Function to create a new user
+    :param username:
+    :param password:
+    :param email:
+    :return:
+    """
+    new_user = User(username, password, email)
+    return new_user
+
+
+
