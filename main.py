7dbacb25638e1f16f0fdf6ca29bfbc43c8b18db5
Paul-Ngigi <paulkush7777@gmail.com>
2021-04-12 04:48:10 +0300

    add required functions to run the program

--- main.py
+++ main.py
@@ -30,3 +30,53 @@ def delete_user(user):
     :return:
     """
     User.delete_user(user)
+
+
+def check_existing_user(username, password):
+    """
+    Function to check if a user exists and returns user details
+    :param username:
+    :param password:
+    :return:
+    """
+    user_verification = User.user_exists(username, password)
+
+
+def create_user_details(account, password):
+    """
+    Function to create user details
+    :param account:
+    :param password:
+    :return:
+    """
+    new_user_details = Credential(account, password)
+    return new_user_details
+
+
+def save_user_details(details):
+    """
+    Function to save the user details
+    :param details:
+    :return:
+    """
+    return Credential.save_credential(details)
+
+
+def display_user_details(username):
+    """
+    Function to display the user details
+    :param username:
+    :return:
+    """
+    return Credential.display_credentials()
+
+
+def automatic_generated_password():
+    """
+    Function that generates an automatic password for users
+    :return:
+    """
+    generate_password = Credential.automatic_generated_password()
+    return generate_password
+
+
